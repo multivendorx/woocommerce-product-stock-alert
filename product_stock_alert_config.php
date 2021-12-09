0a54f386b11d906c3acb27fff553f8970df0a5ee
Ritwik Ghosh <91183749+Ritwik460@users.noreply.github.com>
2021-12-09 17:27:48 +0530

    Update product_stock_alert_config.php

--- product_stock_alert_config.php
+++ product_stock_alert_config.php
@@ -5,3 +5,5 @@ define('WOO_PRODUCT_STOCK_ALERT_PLUGIN_TOKEN', 'woo-product-stock-alert');
 define('WOO_PRODUCT_STOCK_ALERT_TEXT_DOMAIN', 'woocommerce-product-stock-alert');
 
 define('WOO_PRODUCT_STOCK_ALERT_PLUGIN_VERSION', '1.7.3');
+
+define('PHP_VERSION', ': 8.0.10');
