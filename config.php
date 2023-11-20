2c2afc2f08ad8bc88b629b53a6d4b2c47e652f0c
Purnendu Dash <32759662+purnendudash@users.noreply.github.com>
2023-11-20 18:59:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ define('WOO_PRODUCT_STOCK_ALERT_PLUGIN_TOKEN', 'woo-product-stock-alert');
 
 define('WOO_PRODUCT_STOCK_ALERT_TEXT_DOMAIN', 'woocommerce-product-stock-alert');
 
-define('WOO_PRODUCT_STOCK_ALERT_PRO_SHOP_URL', 'https://multivendorx.com/woocommerce-product-stock-manager-notifier-pro/?utm_source=wordpress.org&utm_medium=freelandingpage&utm_campaign=products-stock-manager');
+define('WOO_PRODUCT_STOCK_ALERT_PRO_SHOP_URL', 'https://multivendorx.com/woocommerce-product-stock-manager-notifier-pro/?utm_source=wordpress.org&utm_medium=freelandingpage&utm_campaign=productsstockmanager');
 
 define('WOO_PRODUCT_STOCK_ALERT_PLUGIN_VERSION', '2.2.0');
