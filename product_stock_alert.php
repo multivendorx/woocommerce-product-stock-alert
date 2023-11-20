26104566e71deb91c70e94b3219d26812694de38
moumitahalder <moumita.h@dualcube.com>
2023-11-20 18:36:26 +0530

    Update product_stock_alert.php

--- product_stock_alert.php
+++ product_stock_alert.php
@@ -44,7 +44,7 @@ add_filter('plugin_action_links_' . plugin_basename( __FILE__ ), 'woo_product_st
 function woo_product_stock_alert_settings($links) {
     $plugin_links = array(
         '<a href="' . admin_url('admin.php?page=woo-stock-alert-setting#&tab=settings&subtab=general') . '">' . __('Settings', 'woocommerce-product-stock-alert') . '</a>',
-        '<a href="https://multivendorx.com/support-forum/woocommerce-product-stock-alert/?utm_source=wordpress.org&utm_medium=freelandingpage&utm_campaign=products-stock-manager">' . __('Support', 'woocommerce-product-stock-alert') . '</a>',
+        '<a href="https://multivendorx.com/support-forum/forum/product-stock-manager-notifier-for-woocommerce/?utm_source=wordpress.org&utm_medium=freelandingpage&utm_campaign=products-stock-manager">' . __('Support', 'woocommerce-product-stock-alert') . '</a>',
         '<a href="https://multivendorx.com/docs/knowledgebase/products-stock-manager-notifier-for-woocommerce/?utm_source=wordpress.org&utm_medium=freelandingpage&utm_campaign=products-stock-manager">' . __('Docs', 'woocommerce-product-stock-alert') . '</a>'
     );
     if (apply_filters('is_stock_alert_pro_inactive', true)) {
