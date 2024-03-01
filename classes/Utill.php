b79992941ddae8fa6d0056b3e2040ac6d969ae76
Pravat Sahoo <pravats459@gmail.com>
2024-03-01 21:26:21 +0530

    migration problem fixed

--- classes/Utill.php
+++ classes/Utill.php
@@ -33,38 +33,54 @@ class Utill {
     }
 
     /**
+     * Html for database migration notice.
+     * @return void
+     */
+    public static function database_migration_notice() {
+        // check if plugin vertion in databse is not same to current stock manager version
+        $plugin_version = get_option( 'woo_stock_manager_version', '' );
+        if ( $plugin_version != WOO_STOCK_MANAGER_PLUGIN_VERSION ) {
+            ?>
+            <div id="message" class="error">
+                <p><?php _e( "The Product Stock Manager & Notifier for WooCommerce is experiencing configuration issues. To ensure proper functioning, kindly deactivate and then activate the plugin." ) ?></p>
+            </div>
+            <?php
+        }
+    }
+
+    /**
      * Get the settings arry. Non set value is replaced with default value.
      * @return array
      */
     public static function get_form_settings_array() {
-        $general_tab_settings = get_option('woo_stock_manager_general_tab_settings');
-        $form_customization_tab_settings = get_option('woo_stock_manager_form_customization_tab_settings');
-        $form_submission_tab_settings = get_option('woo_stock_manager_form_submission_tab_settings');
-        $email_tab_settings = get_option('woo_stock_manager_email_tab_settings');
+        $general_tab_settings = get_option('woo_stock_manager_general_tab_settings', []);
+        $form_customization_tab_settings = get_option('woo_stock_manager_form_customization_tab_settings', []);
+        $form_submission_tab_settings = get_option('woo_stock_manager_form_submission_tab_settings', []);
+        $email_tab_settings = get_option('woo_stock_manager_email_tab_settings', []);
         
         return [
-            'double_opt_in_success'             => $general_tab_settings['double_opt_in_success'],
-            'shown_interest_text'               => $general_tab_settings['shown_interest_text'],
-            'alert_success'                     => $form_submission_tab_settings['alert_success'],
-            'alert_email_exist'                 => $form_submission_tab_settings['alert_email_exist'],
-            'valid_email'                       => $form_submission_tab_settings['valid_email'],
-            'alert_unsubscribe_message'         => $form_submission_tab_settings['alert_unsubscribe_message'],
-            'email_placeholder_text'            => $form_customization_tab_settings['email_placeholder_text'],
-            'alert_text'                        => $form_customization_tab_settings['alert_text'],
-            'button_text'                       => $form_customization_tab_settings['button_text'],
-            'unsubscribe_button_text'           => $form_customization_tab_settings['unsubscribe_button_text'],
-            'alert_text_color'                  => $form_customization_tab_settings['alert_text_color'],
-            'button_background_color'           => $form_customization_tab_settings['button_background_color'],
-            'button_border_color'               => $form_customization_tab_settings['button_border_color'],
-            'button_text_color'                 => $form_customization_tab_settings['button_text_color'],
-            'button_background_color_onhover'   => $form_customization_tab_settings['button_background_color_onhover'],
-            'button_text_color_onhover'         => $form_customization_tab_settings['button_text_color_onhover'],
-            'button_border_color_onhover'       => $form_customization_tab_settings['button_border_color_onhover'],
-            'button_font_size'                  => $form_customization_tab_settings['button_font_size'],
-            'button_border_size'                => $form_customization_tab_settings['button_border_size'],
-            'button_border_radious'             => $form_customization_tab_settings['button_border_radious'],
-            'ban_email_domain_text'             => $email_tab_settings['ban_email_domain_text'],
-            'ban_email_address_text'            => $email_tab_settings['ban_email_address_text'],
+            'double_opt_in_success'             => $general_tab_settings['double_opt_in_success'] ?? '',
+            'shown_interest_text'               => $general_tab_settings['shown_interest_text'] ?? '',
+            'alert_success'                     => $form_submission_tab_settings['alert_success'] ?? '',
+            'alert_email_exist'                 => $form_submission_tab_settings['alert_email_exist'] ?? '',
+            'valid_email'                       => $form_submission_tab_settings['valid_email'] ?? '',
+            'alert_unsubscribe_message'         => $form_submission_tab_settings['alert_unsubscribe_message'] ?? '',
+            'email_placeholder_text'            => $form_customization_tab_settings['email_placeholder_text'] ?? '',
+            'alert_text'                        => $form_customization_tab_settings['alert_text'] ?? '',
+            'button_text'                       => $form_customization_tab_settings['button_text'] ?? '',
+            'unsubscribe_button_text'           => $form_customization_tab_settings['unsubscribe_button_text'] ?? '',
+            'alert_text_color'                  => $form_customization_tab_settings['alert_text_color'] ?? '',
+            'button_background_color'           => $form_customization_tab_settings['button_background_color'] ?? '',
+            'button_border_color'               => $form_customization_tab_settings['button_border_color'] ?? '',
+            'button_text_color'                 => $form_customization_tab_settings['button_text_color'] ?? '',
+            'button_background_color_onhover'   => $form_customization_tab_settings['button_background_color_onhover'] ?? '',
+            'button_text_color_onhover'         => $form_customization_tab_settings['button_text_color_onhover'] ?? '',
+            'button_border_color_onhover'       => $form_customization_tab_settings['button_border_color_onhover'] ?? '',
+            'button_font_size'                  => $form_customization_tab_settings['button_font_size'] ?? '',
+            'button_border_size'                => $form_customization_tab_settings['button_border_size'] ?? '',
+            'button_border_radious'             => $form_customization_tab_settings['button_border_radious'] ?? '',
+            'ban_email_domain_text'             => $email_tab_settings['ban_email_domain_text'] ?? '',
+            'ban_email_address_text'            => $email_tab_settings['ban_email_address_text'] ?? '',
         ];
     }
 }
