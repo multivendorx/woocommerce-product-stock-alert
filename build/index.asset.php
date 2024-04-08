f435d261980cf07fa60348cdbaf7469e0967fc1b
Bishal221 <kayalbishal221@gmail.com>
2024-04-08 11:07:30 +0530

    Remove submenu from setting panel
    Remove submenu from setting panel

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '67741c9b9d3fd98ac24a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'b5006dbce77f6f1a3053');
