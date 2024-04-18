04ebc2ef5cb0f6f4d30ef0389aa40a431bcc0dfa
moumitahalder <moumita.h@dualcube.com>
2024-04-18 14:03:19 +0530

    Update mailchimp.js

--- src/template/settings/mailchimp.js
+++ src/template/settings/mailchimp.js
@@ -4,7 +4,7 @@ export default {
     id: "mailchimp",
     priority: 5,
     name: __("Mailchimp Integration", "woocommerce-stock-manager"),
-    desc: __("Configure mailChimp settings. ", "woocommerce-stock-manager"),
+    desc: __("Connect your website with Mailchimp, to transfer of user data for email marketing purposes.", "woocommerce-stock-manager"),
     icon: "font-mailchimp",
     proDependent: true,
     submitUrl: "save-stockmanager",
