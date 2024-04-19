0c19af362c7a652a1c573e229c88f67227686c38
Bishal221 <71867054+Bishal221@users.noreply.github.com>
2024-04-19 19:44:07 +0530

    Pro tag fix
    Pro tag fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '8d518ec24cd6b84e113e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '2970e0146ebc0affc956');
