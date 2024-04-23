05f754e8dd015f6ac3fed8340f7c9ce530036a0a
Bishal221 <kayalbishal221@gmail.com>
2024-04-23 17:33:43 +0530

    Tab menu fix
    Tab menu fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'a97b6a5dfa608aa7d126');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e256411bbf1b227d35f8');
