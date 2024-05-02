7924cca8436d87f7561f7cd18b37c7a80965a58f
Bishal221 <kayalbishal221@gmail.com>
2024-05-02 16:24:29 +0530

    R suite code remove

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'caa5c0088763899fa834');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '43eaa9761b1ded83d620');
