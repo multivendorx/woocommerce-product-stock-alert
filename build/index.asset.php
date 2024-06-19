23c5b4dc8714e9e0aa491b80c7de9625cf18c844
Bishal221 <kayalbishal221@gmail.com>
2024-06-19 13:44:21 +0530

    Manage stock table enhancement

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '17c3f6cc9a63439849d5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '11988acd3eb292a22db1');
