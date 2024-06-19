6558609d8b6edc027f01a040bc40de6fe1333a4a
Vivodia <github@vivodia.com>
2024-06-19 19:25:58 +0200

    Update SubscriberConfirmationEmail.php
    Inversion between including and excluding tax !!!

--- templates/emails/SubscriberConfirmationEmail.php
+++ templates/emails/SubscriberConfirmationEmail.php
@@ -30,7 +30,7 @@ $is_prices_including_tax = get_option( 'woocommerce_prices_include_tax' );
 			<th scope="col" style="text-align:left; border: 1px solid #eee;">
 				<?php 
 					echo wp_kses_post( wc_price( wc_get_price_to_display( $product ) ) ); 
-					echo ( isset( $is_prices_including_tax ) && ( $is_prices_including_tax != "yes" ) ) ? WC()->countries->ex_tax_or_vat() : WC()->countries->inc_tax_or_vat(); 
+					echo ( isset( $is_prices_including_tax ) && ( $is_prices_including_tax != "yes" ) ) ? WC()->countries->inc_tax_or_vat() : WC()->countries->ex_tax_or_vat(); 
 				?>
 			</th>
 		</tr>
