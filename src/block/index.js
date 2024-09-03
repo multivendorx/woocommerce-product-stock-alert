4f3b3f2d294a46a8ab8e34813ecd04f20063de14
OendrilaC <oendrila.chakraborty2003@gmail.com>
2024-09-03 18:39:05 +0530

    shortcode to block

---
+++ src/block/index.js
@@ -0,0 +1,28 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps } from '@wordpress/block-editor';
+
+registerBlockType('stock-manager/stock-manager-form', {
+    title: 'Stock Manager Form',
+    description: 'A block to display the Stock Manager form.',
+    category: 'widgets',
+    icon: 'clipboard',
+    supports: {
+        html: false,
+    },
+
+    edit: () => {
+        return (
+            <div {...useBlockProps()}>
+                <p>Stock Manager Form Block</p>
+            </div>
+        );
+    },
+
+    save: () => {
+        return (
+            <div {...useBlockProps.save()}>
+                <p>[display_stock_manager_form]</p>
+            </div>
+        );
+    },
+});
