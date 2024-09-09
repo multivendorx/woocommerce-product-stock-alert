4f63d8f489dc1b440d54c6f856432cac900f7dc5
OendrilaC <115146285+OendrilaC@users.noreply.github.com>
2024-09-09 12:46:48 +0530

    Update Utill.php

--- classes/Utill.php
+++ classes/Utill.php
@@ -38,51 +38,8 @@ class Utill {
      * @return array
      */
     public static function get_form_settings_array() {
+        // Initialize the settings keys with default values
         $setting_keys = [
-            'double_opt_in_success',
-            'shown_interest_text',
-            'alert_success',
-            'alert_email_exist',
-            'valid_email',
-            'alert_unsubscribe_message',
-            'email_placeholder_text',
-            'alert_text',
-            'button_text',
-            'unsubscribe_button_text',
-            'alert_text_color',
-            'button_background_color',
-            'button_border_color',
-            'button_text_color',
-            'button_background_color_onhover',
-            'button_text_color_onhover',
-            'button_border_color_onhover',
-            'button_font_size',
-            'button_border_size',
-            'button_border_radious',
-            'ban_email_domain_text',
-            'ban_email_address_text',
-        ];
-    
-        $form_settings = [];
-    
-        foreach ( $setting_keys as $key ) {
-            $default_value = self::get_default_setting( $key ); // You can modify this to get the default value if needed.
-    
-            // Register string using WPML's icl_register_string function
-            if ( function_exists( 'icl_register_string' ) ) {
-                icl_register_string( 'woocommerce-stock-manager', $key, $default_value );
-            }
-    
-            // Store registered or default value in settings array
-            $form_settings[ $key ] = function_exists( 'icl_t' ) ? icl_t( 'woocommerce-stock-manager', $key, $default_value ) : $default_value;
-        }
-    
-        return $form_settings;
-    }
-    
-    // Function to get default values for each key
-    public static function get_default_setting( $key ) {
-        $defaults = [
             'double_opt_in_success' => __( 'Kindly check your inbox to confirm the subscription.', 'woocommerce-stock-manager' ),
             'shown_interest_text' => __( 'Thank you for your interest.', 'woocommerce-stock-manager' ),
             'alert_success' => __( 'Thank you for expressing interest in %product_title%. We will notify you once it is back in stock.', 'woocommerce-stock-manager' ),
@@ -93,24 +50,36 @@ class Utill {
             'alert_text' => __( 'Receive notifications when back in stock.', 'woocommerce-stock-manager' ),
             'button_text' => __( 'Notify me', 'woocommerce-stock-manager' ),
             'unsubscribe_button_text' => __( 'Unsubscribe', 'woocommerce-stock-manager' ),
-            'alert_text_color' => '', // Add appropriate defaults if required
-            'button_background_color' => '',
-            'button_border_color' => '',
-            'button_text_color' => '',
-            'button_background_color_onhover' => '',
-            'button_text_color_onhover' => '',
-            'button_border_color_onhover' => '',
-            'button_font_size' => '',
-            'button_border_size' => '',
-            'button_border_radious' => '',
+            'alert_text_color' => '', // Default value
+            'button_background_color' => '', // Default value
+            'button_border_color' => '', // Default value
+            'button_text_color' => '', // Default value
+            'button_background_color_onhover' => '', // Default value
+            'button_text_color_onhover' => '', // Default value
+            'button_border_color_onhover' => '', // Default value
+            'button_font_size' => '', // Default value
+            'button_border_size' => '', // Default value
+            'button_border_radious' => '', // Default value
             'ban_email_domain_text' => __( 'This email domain is not allowed.', 'woocommerce-stock-manager' ),
             'ban_email_address_text' => __( 'This email address is banned.', 'woocommerce-stock-manager' ),
         ];
     
-        return isset( $defaults[ $key ] ) ? $defaults[ $key ] : '';
+        $form_settings = [];
+    
+        foreach ( $setting_keys as $key => $default_value ) {
+            // Register string using WPML's icl_register_string function if available
+            if ( function_exists( 'icl_register_string' ) ) {
+                icl_register_string( 'woocommerce-stock-manager', $key, $default_value );
+            }
+    
+            // Store registered or default value in settings array
+            $form_settings[ $key ] = function_exists( 'icl_t' ) 
+                ? icl_t( 'woocommerce-stock-manager', $key, $default_value ) 
+                : $default_value;
+        }
+    
+        return $form_settings;
     }
-   
-
     /**
      * Check pro plugin is acrive or not
      * @return bool
