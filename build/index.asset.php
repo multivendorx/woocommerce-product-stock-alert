eac917563b3d7d18c59f79f24d343eaa8dd6a79d
Promita Ghosh <118420070+user-promi@users.noreply.github.com>
2024-10-21 14:20:29 +0530

    updated

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '17d8ca62155233326f42');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '8a1b75dcc15790bc1b9b');
--- src/components/AdminLibrary/Tabs/Tabs.jsx
+++ src/components/AdminLibrary/Tabs/Tabs.jsx
@@ -3,6 +3,7 @@ import Brand from "../../../assets/images/Brand.png";
 import BrandSmall from "../../../assets/images/Brand-small.png";
 import "./tabs.scss";
 import { useState } from "react";
+import AdminFooter from "../../AdminFooter/AdminFooter";
 
 const Tabs = (props) => {
   const {
@@ -45,27 +46,6 @@ const Tabs = (props) => {
       </Link>
     );
   };
-
-  const supportLink = [
-    {
-      title: "Get in touch with Support",
-      icon: "mail",
-      description: "Reach out to the support team for assistance or guidance.",
-      link: "https://multivendorx.com/contact-us/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=stockmanager",
-    },
-    {
-      title: "Explore Documentation",
-      icon: "submission-message",
-      description: "Understand the plugin and its settings.",
-      link: "https://multivendorx.com/docs/knowledgebase/products-stock-manager-notifier-for-woocommerce/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=stockmanager",
-    },
-    {
-      title: "Contribute Here",
-      icon: "support",
-      description: "To participation in product enhancement.",
-      link: "https://github.com/multivendorx/woocommerce-product-stock-alert/issues",
-    },
-  ];
 
   const showHideMenu = (tab) => {
     return (
@@ -147,7 +127,7 @@ const Tabs = (props) => {
               <div className="brand">
                 <img className="logo" src={menuCol ? BrandSmall : Brand} alt="Logo" />
                 <img className="logo-small" src={BrandSmall} alt="Logo" />
-                {menuCol ? null : <p>Stock Manager</p>}
+                {menuCol ? null : <p>{appLocalizer.tab_name}</p>}
               </div>
               <div className="current-tab-lists-container">
                 {tabData.map(({ type, content }) => {
@@ -190,21 +170,7 @@ const Tabs = (props) => {
           </div>
         </div>
 
-        <div className="support-card">
-          {supportLink.map((item, index) => {
-            return (
-              <>
-                <a href={item.link} target="_blank" className="card-item">
-                  <i className={`admin-font font-${item.icon}`}></i>
-                  <a href={item.link} target="_blank">
-                    {item.title}
-                  </a>
-                  <p>{item.description}</p>
-                </a>
-              </>
-            );
-          })}
-        </div>
+        <AdminFooter/>
       </div>
     </>
   );
